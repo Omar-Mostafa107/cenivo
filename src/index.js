ab4c7dc9a5051dd66490b21ce9e83cd2f821f3ef
Omar Mostafa <omarmostafa8978@gmail.com>
2026-01-11 19:53:40 +0200

    Clean up index.js by removing unused code
    Removed unused imports and commented-out StarRating components.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import StarRating from "./StarRating";
 
 function Test() {
   return (
@@ -15,18 +14,6 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <StarRating
-      maxRating={5}
-      message={["Terrible", "Bad", "Okay", "Good", "Great"]}
-    />
-    <StarRating
-      maxRating={5}
-      color="red"
-      size={24}
-      className="test"
-      defaultRating={3}
-    /> */}
     <Test />
   </React.StrictMode>
 );
